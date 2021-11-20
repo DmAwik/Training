0664539ee38ef8cf8393364b8bb681b5f9dc2ae7
DmAwik <Awii15@mail.ru>
2021-11-20 14:50:05 +0300

    Task01

---
+++ epam/training/calculator/src/main/java/com/epam/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package com.epam.calculator;
+
+public class Calculator {
+ public double add(double firstArg, double secondArg){
+
+    return firstArg+secondArg;
+ }
+    public double subtract(double firstArg, double secondArg){
+
+        return firstArg-secondArg;
+    }
+    public double multiply(double firstArg, double secondArg){
+
+        return firstArg*secondArg;
+    }
+    public double divide(double firstArg, double secondArg){
+
+        return firstArg/secondArg;
+    }
+}
