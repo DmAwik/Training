55dc2ddc2f289816d9e5e4ee34833e20f3a27474
DmAwik <Awii15@mail.ru>
2021-11-20 17:11:50 +0300

    Task01

--- epam/training/calculator/src/test/java/com/epam/calculator/CalculatorTest.java
+++ epam/training/calculator/src/test/java/com/epam/calculator/CalculatorTest.java
@@ -3,56 +3,71 @@ package com.epam.calculator;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Random;
-
 public class CalculatorTest {
 
     @Test
-    public void testAddShouldAddWhenNumberRandom(){
+    public void testAddShouldAddWhenNumbersPositive(){
         Calculator calculator = new Calculator();
-        Random random = new Random();
-        double firstArg= random.nextDouble();
-        double secondArg= random.nextDouble();
-        double sum=firstArg+secondArg;
 
-        double result=calculator.add(firstArg,secondArg);
+        double result=calculator.add(8.17d,11.2d);
 
-        Assert.assertEquals(sum,result,0.01);
+        Assert.assertEquals(19.37d,result,0.01);
     }
     @Test
-    public void testSubtractShouldSubtractWhenNumberRandom(){
+    public void testAddShouldAddWhenNumbersNegative(){
         Calculator calculator = new Calculator();
-        Random random = new Random();
-        double firstArg= random.nextDouble();
-        double secondArg= random.nextDouble();
-        double subtraction=firstArg-secondArg;
 
-        double result=calculator.subtract(firstArg,secondArg);
+        double result=calculator.add(-8.17d,-11.2d);
 
-        Assert.assertEquals(subtraction,result,0.01);
+        Assert.assertEquals(-19.37d,result,0.01);
     }
     @Test
-    public void testMultiplyShouldMultiplyWhenNumberRandom(){
+    public void testSubtractShouldSubtractWhenNumbersPositive(){
         Calculator calculator = new Calculator();
-        Random random = new Random();
-        double firstArg= random.nextDouble();
-        double secondArg= random.nextDouble();
-        double multiplication=firstArg*secondArg;
 
-        double result=calculator.multiply(firstArg,secondArg);
+        double result=calculator.subtract(5.43d,7.56d);
 
-        Assert.assertEquals(multiplication,result,0.01);
+        Assert.assertEquals(-2.13d,result,0.01);
     }
     @Test
-    public void testDivideShouldDivideWhenNumberRandom(){
+    public void testSubtractShouldSubtractWhenNumbersNegative(){
         Calculator calculator = new Calculator();
-        Random random = new Random();
-        double firstArg= random.nextDouble();
-        double secondArg= random.nextDouble();
-        double division=firstArg/secondArg;
 
-        double result=calculator.divide(firstArg,secondArg);
+        double result=calculator.subtract(-5.43d,-7.56d);
 
-        Assert.assertEquals(division,result,0.01);
+        Assert.assertEquals(2.13d,result,0.01);
+    }
+
+    @Test
+    public void testMultiplyShouldMultiplyWhenNumbersPositive(){
+        Calculator calculator = new Calculator();
+
+        double result=calculator.multiply(2.5d,2.5d);
+
+        Assert.assertEquals(6.25d,result,0.01);
+    }
+    @Test
+    public void testMultiplyShouldMultiplyWhenOneNumberPositiveOneNegative(){
+        Calculator calculator = new Calculator();
+
+        double result=calculator.multiply(2.5d,-2.5d);
+
+        Assert.assertEquals(-6.25d,result,0.01);
+    }
+    @Test
+    public void testDivideShouldDivideWhenNumbersPositive(){
+        Calculator calculator = new Calculator();
+
+        double result=calculator.divide(42.2d,2d);
+
+        Assert.assertEquals(21.1d,result,0.01);
+    }
+    @Test
+    public void testDivideShouldDivideWhenOneNumberPositiveOneNumberNegative(){
+        Calculator calculator = new Calculator();
+
+        double result=calculator.divide(42.2d,-2d);
+
+        Assert.assertEquals(-21.1d,result,0.01);
     }
 }
